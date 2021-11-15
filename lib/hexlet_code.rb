d0f14649287ada98b2d1addf1976ef7fcf54ad39
Vasiliy Chapaev <morphizm@ya.ru>
2021-11-15 03:31:41 +0300

    add labels, input, submit methods

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -2,8 +2,21 @@
 
 require_relative 'hexlet_code/version'
 require_relative 'hexlet_code/tag'
+require_relative 'hexlet_code/form_for'
 
 module HexletCode
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.form_for(instance, attributes = {})
+    method = attributes.fetch(:method, 'post')
+    url = attributes.fetch(:url, '#')
+
+    result = yield HexletCode::FormFor.new(instance)
+    if result
+      body = result.join("\n")
+      "<form action=\"#{url}\" method=\"#{method}\">\n#{body}\n</form>"
+    else
+      "<form action=\"#{url}\" method=\"#{method}\">\n</form>"
+    end
+  end
 end
---
+++ test/form_for_test.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class FormForTest < Minitest::Test
+  User = Struct.new(:name, :job, :gender, keyword_init: true)
+
+  def test_form_for_without_fields
+    user = User.new name: 'rob'
+
+    result = HexletCode.form_for user do |f|
+    end
+    expected = "<form action=\"#\" method=\"post\">\n</form>"
+    assert_equal expected, result
+
+    result2 = HexletCode.form_for user, url: '/users' do |f|
+    end
+    expected2 = "<form action=\"/users\" method=\"post\">\n</form>"
+    assert_equal expected2, result2
+  end
+
+  def test_with_fields
+    user = User.new name: 'rob', job: 'hexlet', gender: 'm'
+    result = HexletCode.form_for user do |f|
+      f.input :name
+      f.input :job, as: :text
+      f.submit
+    end
+    expected = "\
+<form action=\"#\" method=\"post\">\n\
+<label for=\"name\">Name</label>\n\
+<input name=\"name\" type=\"text\" value=\"rob\">\n\
+<label for=\"job\">Job</label>\n\
+<textarea cols=\"20\" rows=\"40\" name=\"job\">hexlet</textarea>\n\
+<input name=\"commit\" type=\"submit\" value=\"Save\">
+</form>"
+
+    assert_equal expected, result
+  end
+
+  def test_field_error_on_no_method_instance
+    user = User.new name: 'rob', job: 'hexlet', gender: 'm'
+    assert_raises(NoMethodError) do
+      HexletCode.form_for user, url: '/users' do |f|
+        f.input :name
+        f.input :job, as: :text
+        # user does not have age field
+        f.input :age
+        f.submit
+      end
+    end
+  end
+end
